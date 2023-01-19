27f25cb10178d987bde37b7682fef991f8c562e8
Unit Zero-Three <unitzerothree@gmail.com>
2023-01-19 13:01:13 +1100

    Initial solution

---
+++ Anodize/Anodize/MainForm.cs
@@ -0,0 +1,15 @@
+using System;
+
+using Eto.Drawing;
+using Eto.Forms;
+
+namespace Anodize
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ Anodize/Anodize/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Eto.Drawing;
+using Eto.Forms;
+
+namespace Anodize
+{
+	internal class Program
+	{
+		[STAThread]
+		static void Main(string[] args)
+		{
+			new Application(Eto.Platform.Detect).Run(new MainForm());
+		}
+	}
+}
